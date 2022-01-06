d17edc734a6ce85c3f4b4d3926cdbf5345d795e2
Ates <jetates@gmail.com>
2022-01-07 00:35:52 +0300

    first ios release bugs fixed

--- RandomPhotoRemover/App.js
+++ RandomPhotoRemover/App.js
@@ -21,10 +21,12 @@ import {
   useWindowDimensions,
   Alert,
   ActivityIndicator,
+  Pressable,
 } from 'react-native';
 
-import CameraRoll from "@react-native-community/cameraroll";
+import CameraRoll, { deletePhotos } from "@react-native-community/cameraroll";
 
+import RNShake from 'react-native-shake';
 
 import { launchImageLibrary } from 'react-native-image-picker';
 var RNFS = require('react-native-fs');
@@ -34,6 +36,7 @@ var RNFS = require('react-native-fs');
 const App: () => Node = () => {
   const colorScheme = useColorScheme();
   const [loading, setLoading] = useState(true);
+  const [randomLoading, setRandomLoading] = useState(false);
   const [img, setImg] = useState('{"uri": "/assets/galeryImages.jpg","width":1000,"height":1000}');
   const defaultUri = "/assets/galeryImages.jpg";
   const windowWidth = useWindowDimensions().width;
@@ -49,16 +52,16 @@ const App: () => Node = () => {
 
   getRandomImage = async () => {
 
-    previousRandom = 0;
+    setRandomLoading(true)
+    // previousRandom = 0;
     let random = 0;
 
-    while (random === previousRandom) {
-      random = Math.floor(Math.random() * images.length)
-    }
+    // while (random === previousRandom) {
+    random = Math.floor(Math.random() * images.length)
+    // }
 
     let img = images[random];
 
-    isPhotoInPortraitMode = true;
 
 
 
@@ -68,36 +71,67 @@ const App: () => Node = () => {
 
 
     if (imageRatio > displayRatio) {
-      img.width = windowWidth;
+      img.width = windowWidth * 90 / 100;
       img.height = img.width / imageRatio;
     }
     else {
-      img.height = windowHeight - 200;
+      img.height = windowHeight * 88 / 100;
       img.width = img.height * imageRatio;
     }
 
 
     setImg(img);
+    setRandomLoading(false);
 
   }
 
   deleteImage = async () => {
-    CameraRoll.deletePhotos([img.uri])
-      .then((data) => {
-        console.log('data', data);
-        setImages(images.filter(s => s != img));
-        console.log(images.length);
-        getRandomImage();
-      })
-      .catch((error) => {
-        console.log("rejected ", error.message);
-      });
+
+    if (images.length > 0) {
+
+      CameraRoll.deletePhotos([img.uri])
+        .then((data) => {
+          console.log('data', data);
+          setImages(images.filter(s => s != img));
+          console.log(images.length);
+          getRandomImage();
+        })
+        .catch((error) => {
+          console.log("rejected ", error.message);
+        });
+    }
   }
 
 
   getAllPhotos = () => {
-    CameraRoll.getPhotos({ first: 10000000000, assetType: 'Photos' })
+
+    let getCount = 100000000
+
+
+    if (__DEV__) {
+      getCount = 10
+    }
+
+    CameraRoll.getPhotos({ first: getCount, assetType: 'Photos' })
       .then(({ edges }) => {
+
+        if (__DEV__) {
+          edges = edges.concat(edges);
+          edges = edges.concat(edges);
+          edges = edges.concat(edges);
+          edges = edges.concat(edges);
+          edges = edges.concat(edges);
+          edges = edges.concat(edges);
+          edges = edges.concat(edges);
+          edges = edges.concat(edges);
+          edges = edges.concat(edges);
+          edges = edges.concat(edges);
+          edges = edges.concat(edges);
+          edges = edges.concat(edges);
+          edges = edges.concat(edges);
+        }
+
+
         setImages(edges.map((edge) => edge.node.image));
         getRandomImage();
         setLoading(false);
@@ -107,6 +141,16 @@ const App: () => Node = () => {
 
   useEffect(() => {
     getAllPhotos();
+
+    const subscription = RNShake.addListener(() => {
+      deleteImage();
+    })
+
+    return () => {
+      subscription.remove()
+    }
+
+
   }, []);
 
 
@@ -116,25 +160,43 @@ const App: () => Node = () => {
       {loading ?
         <View style={[styles.container, { height: windowHeight / 2 }]}>
           <ActivityIndicator></ActivityIndicator>
-          <Text style={styles.loadingText}>Your photos are getting ready!</Text>
+          <Text style={styles.loadingText}></Text>
+          <Text style={styles.loadingText}>Files are getting ready! It can take a while</Text>
+          <Text style={styles.loadingText}>When it loaded you can tap to change photo</Text>
+          <Text style={styles.loadingText}>{"<<"}Shake the device to delete photo on the screen{">>"}</Text>
         </View>
         :
-        <View style={[styles.container, { height: windowHeight / 2 }]}>
-          <Text style={styles.foundCount}>{images.length} photos found</Text>
-          <Image
-            style={{ width: img.width, height: img.height }}
-            // style={{ width: (img.width / (img.height / windowHeight / 2)), height: windowHeight / 2 }}
-            // style={{ width: '50%', height: (img.height / (img.width / windowWidth)) / 2 }}
-            source={{ uri: img ? img.uri : defaultUri }}
-          />
-          <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-between' }}>
-            <Button title="Delete" onPress={() => deleteImage()} />
-            <Button title="Skip" onPress={() => getRandomImage()} />
+        <View style={[styles.container, { height: windowHeight / 2 }]} >
+
+          <Pressable style={{ flex: 50 }} onPress={() => getRandomImage()} >
+            <View style={{
+              alignSelf: 'center', top: 0, left: 0, position: 'absolute', zIndex: 99,
+              borderTopLeftRadius: 5, borderBottomRightRadius: 5, borderTopWidth: 1, borderLeftWidth: 1, borderColor: '#383838',
+              backgroundColor: 'white', opacity: 1
+            }}>
+              <Image source={require('./assets/PhotoCleaner.png')} style={{ width: 80, height: 35, }}></Image>
+            </View>
+            {randomLoading ?
+              <View style={[styles.container, { height: windowHeight / 2 }]}>
+                <ActivityIndicator></ActivityIndicator>
+                <Text style={styles.loadingText}>Your photos are getting ready!</Text>
+              </View>
+              :
+              <Image
+                style={{
+                  width: img.width, height: img.height, borderRadius: 5, borderWidth: 1, borderColor: '#383838'
+                }}
+                source={{ uri: img ? img.uri : defaultUri }}
+              />
+            }
+          </Pressable>
+          <View style={{ flex: 1 }}>
+            <Text style={styles.shake2Delete}>{images.length} photos found. Tap to change or shake to delete this photo. It will ask permission</Text>
           </View>
 
         </View>
       }
-    </SafeAreaView>
+    </SafeAreaView >
   );
 };
 
@@ -159,7 +221,17 @@ const darkThemeStyles = StyleSheet.create({
     color: 'white'
   },
   foundCount: {
-    color: 'white'
+    fontSize: 10,
+    color: 'white',
+  },
+  shake2Delete: {
+    color: 'white',
+    fontSize: 7
+  },
+  shake2DeleteDescription: {
+    color: 'white',
+    fontSize: 9,
+    alignSelf: 'center'
   }
 });
 
@@ -179,8 +251,18 @@ const lightThemeStyles = StyleSheet.create({
   loadingText: {
     color: 'black'
   },
-  foundCount:{
-    color: 'black'
+  foundCount: {
+    fontSize: 10,
+    color: 'black',
+  },
+  shake2Delete: {
+    color: 'black',
+    fontSize: 7,
+  },
+  shake2DeleteDescription: {
+    color: 'black',
+    fontSize: 9,
+    alignSelf: 'center'
   }
 });
 
